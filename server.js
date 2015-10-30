84b6611f3cda5938877bae93d0f270d57eab6758
Victor <victor.dupuy@hei.fr>
2015-10-29 21:05:31 -0400

    Setup server

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+  res.json({
+    x : 1,
+    y : 2
+  });
+});
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000");
+});
