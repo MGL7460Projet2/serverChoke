e7b213ae4558ee6a9b718063e87525ec47f7e7d1
Victor <victor.dupuy@hei.fr>
2015-11-19 17:13:19 -0500

    Essai réussi en Front-end : Récupère les Events de l'utilisateur

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var Facebook = require('facebook-node-sdk');
+var path = require('path');
 
 // Models
 var Events = require('../models/event');
@@ -16,23 +17,30 @@ Events.register(router, '/events');
 Chokes.methods(['get', 'put', 'post', 'delete']);
 Chokes.register(router, '/chokes');
 
-router.get('/index', function(req, res){
-  res.json({
-    content : "Autentication success !"
+  //Fail & Success
+  router.get('/index', function(req, res){
+    res.json({
+      content : "Autentication success !"
+    });
   });
-});
 
-router.get('/fail', function(req, res){
-  res.json({
-    content : "Autentication failed ! "
+  router.get('/fail', function(req, res){
+    res.json({
+      content : "Autentication failed ! "
+    });
+  })
+
+  //Passport
+  router.get('/auth/facebook', passport.authenticate('facebook', {scope : ['email'] } ));
+
+  router.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { successRedirect: '/api/index',
+                                        failureRedirect: '/api/fail' }));
+
+  //Facebook SDK
+  router.get('/facebook', function(req, res){
+    res.sendFile(path.join(__dirname + '/index.html'));
   });
-})
-
-router.get('/auth/facebook', passport.authenticate('facebook', {scope : ['email'] } ));
-
-router.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { successRedirect: '/api/index',
-                                      failureRedirect: '/api/fail' }));
 
 // Return router
 module.exports = router;
