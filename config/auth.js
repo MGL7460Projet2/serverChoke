099a50291aabb17b80be80377dd2e3a8333b83bb
Victor <victor.dupuy@hei.fr>
2015-11-20 12:24:50 -0500

    Correction FB Callback URL to point at Heroku domain

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
   'facebookAuth' : {
     'clientID' : '1093913377287097',
     'clientSecret' : 'd27d458ff5f09cad2bcfabd37729ecae',
-    'callbackURL' : 'http://localhost:3000/api/auth/facebook/callback' 
+    'callbackURL' : 'https://shrouded-harbor-6203.herokuapp.com/api/auth/facebook/callback'
   }
 }
