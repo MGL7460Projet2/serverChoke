e5c6bd6ec2765d908ca544f42b315f0e90834768
Victor <victor.dupuy@hei.fr>
2015-11-20 16:08:57 -0500

    BIG COMMIT : Added routes for fetching Events data from Facebook Graph API. Routes are for : Event photos, videos, main picture, users (attending, declined, interested, noreply, maybe), admins users, users with roles (admin, contacts, etc...) & event data (description, name, location, etc...)

--- routes/api.js
+++ routes/api.js
@@ -52,10 +52,10 @@ router.post('/events/', function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
   });
 
-  //Data handling for user
+/*  **************  Data handling for user  ************** */
   //Here, we need a session. It's EXTREMELY important !
   router.get('/myEvents', function(req, res){
-    sessionID = "10206500617488421"; //TEMPORARY, WE NEED THAT SESSION SYSTEM (cookie?)
+    sessionID = "10206500617488421"; //TEMPORARY, WE NEED THAT SESSION SYSTEM to get the user id & token (cookie?)
     	User.findOne({'fbID': sessionID}, function(err, user){
         if(err){
           console.log(err);
@@ -71,7 +71,8 @@ router.post('/events/', function(req, res){
       });
   });
 
-  router.get('/attendingUsers/:id', function(req, res){
+  // Gives all infos about an event
+  router.get('event/:id', function(req, res){
     var eventID = req.params.id;
     var sessionID = "10206500617488421";
     User.findOne({'fbID': sessionID}, function(err, user){
@@ -93,6 +94,247 @@ router.post('/events/', function(req, res){
       xhr.send();
     });
   });
+
+  //Give all attendingUsers on the event corresponding to the ID in URL
+  router.get('event/attendingUsers/:id', function(req, res){
+    var eventID = req.params.id;
+    var sessionID = "10206500617488421";
+    User.findOne({'fbID': sessionID}, function(err, user){
+      console.log(user.token);
+      var uri = "https://graph.facebook.com/"+eventID+"/attending?access_token="+user.token;
+      console.log(uri);
+
+      // GET Request in native JavaScript
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', encodeURI(uri));
+      xhr.onload = function() {
+          if (xhr.status === 200) {
+              res.json(JSON.parse(xhr.responseText));
+          }
+          else {
+              console.log('Request failed.  Returned status of ' + xhr.status);
+          }
+      };
+      xhr.send();
+    });
+  });
+
+  //Gives all user who denied the Event
+  router.get('event/declined/:id', function(req, res){
+    var eventID = req.params.id;
+    var sessionID = "10206500617488421";
+    User.findOne({'fbID': sessionID}, function(err, user){
+      console.log(user.token);
+      var uri = "https://graph.facebook.com/"+eventID+"/declined?access_token="+user.token;
+      console.log(uri);
+
+      // GET Request in native JavaScript
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', encodeURI(uri));
+      xhr.onload = function() {
+          if (xhr.status === 200) {
+              res.json(JSON.parse(xhr.responseText));
+          }
+          else {
+              console.log('Request failed.  Returned status of ' + xhr.status);
+          }
+      };
+      xhr.send();
+    });
+  });
+
+  //Gives all user who said maybe to the Event
+  router.get('event/maybe/:id', function(req, res){
+    var eventID = req.params.id;
+    var sessionID = "10206500617488421";
+    User.findOne({'fbID': sessionID}, function(err, user){
+      console.log(user.token);
+      var uri = "https://graph.facebook.com/"+eventID+"/maybe?access_token="+user.token;
+      console.log(uri);
+
+      // GET Request in native JavaScript
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', encodeURI(uri));
+      xhr.onload = function() {
+          if (xhr.status === 200) {
+              res.json(JSON.parse(xhr.responseText));
+          }
+          else {
+              console.log('Request failed.  Returned status of ' + xhr.status);
+          }
+      };
+      xhr.send();
+    });
+  });
+
+  //Gives all user who didn't respond to the Event
+  router.get('event/noreply/:id', function(req, res){
+    var eventID = req.params.id;
+    var sessionID = "10206500617488421";
+    User.findOne({'fbID': sessionID}, function(err, user){
+      console.log(user.token);
+      var uri = "https://graph.facebook.com/"+eventID+"/noreply?access_token="+user.token;
+      console.log(uri);
+
+      // GET Request in native JavaScript
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', encodeURI(uri));
+      xhr.onload = function() {
+          if (xhr.status === 200) {
+              res.json(JSON.parse(xhr.responseText));
+          }
+          else {
+              console.log('Request failed.  Returned status of ' + xhr.status);
+          }
+      };
+      xhr.send();
+    });
+  });
+
+  //Gives all user who didn't respond to the Event
+  router.get('event/interested/:id', function(req, res){
+    var eventID = req.params.id;
+    var sessionID = "10206500617488421";
+    User.findOne({'fbID': sessionID}, function(err, user){
+      console.log(user.token);
+      var uri = "https://graph.facebook.com/"+eventID+"/interested?access_token="+user.token;
+      console.log(uri);
+
+      // GET Request in native JavaScript
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', encodeURI(uri));
+      xhr.onload = function() {
+          if (xhr.status === 200) {
+              res.json(JSON.parse(xhr.responseText));
+          }
+          else {
+              console.log('Request failed.  Returned status of ' + xhr.status);
+          }
+      };
+      xhr.send();
+    });
+  });
+
+  //Gives the picture of the Event
+  router.get('event/picture/:id', function(req, res){
+    var eventID = req.params.id;
+    var sessionID = "10206500617488421";
+    User.findOne({'fbID': sessionID}, function(err, user){
+      console.log(user.token);
+      var uri = "https://graph.facebook.com/"+eventID+"/picture?type=large&access_token="+user.token;
+      console.log(uri);
+
+      // GET Request in native JavaScript
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', encodeURI(uri));
+      xhr.onload = function() {
+          if (xhr.status === 200) {
+              res.json(JSON.parse(xhr.responseText));
+          }
+          else {
+              console.log('Request failed.  Returned status of ' + xhr.status);
+          }
+      };
+      xhr.send();
+    });
+  });
+
+  //Gives the photos published on the Event
+  router.get('event/photos/:id', function(req, res){
+    var eventID = req.params.id;
+    var sessionID = "10206500617488421";
+    User.findOne({'fbID': sessionID}, function(err, user){
+      console.log(user.token);
+      var uri = "https://graph.facebook.com/"+eventID+"/photos?access_token="+user.token;
+      console.log(uri);
+
+      // GET Request in native JavaScript
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', encodeURI(uri));
+      xhr.onload = function() {
+          if (xhr.status === 200) {
+              res.json(JSON.parse(xhr.responseText));
+          }
+          else {
+              console.log('Request failed.  Returned status of ' + xhr.status);
+          }
+      };
+      xhr.send();
+    });
+  });
+
+  //Gives the videos published on the Event
+  router.get('event/videos/:id', function(req, res){
+    var eventID = req.params.id;
+    var sessionID = "10206500617488421";
+    User.findOne({'fbID': sessionID}, function(err, user){
+      console.log(user.token);
+      var uri = "https://graph.facebook.com/"+eventID+"/videos?access_token="+user.token;
+      console.log(uri);
+
+      // GET Request in native JavaScript
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', encodeURI(uri));
+      xhr.onload = function() {
+          if (xhr.status === 200) {
+              res.json(JSON.parse(xhr.responseText));
+          }
+          else {
+              console.log('Request failed.  Returned status of ' + xhr.status);
+          }
+      };
+      xhr.send();
+    });
+  });
+
+  //Gives the roles on the Event's staff
+  router.get('event/roles/:id', function(req, res){
+    var eventID = req.params.id;
+    var sessionID = "10206500617488421";
+    User.findOne({'fbID': sessionID}, function(err, user){
+      console.log(user.token);
+      var uri = "https://graph.facebook.com/"+eventID+"/roles?access_token="+user.token;
+      console.log(uri);
+
+      // GET Request in native JavaScript
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', encodeURI(uri));
+      xhr.onload = function() {
+          if (xhr.status === 200) {
+              res.json(JSON.parse(xhr.responseText));
+          }
+          else {
+              console.log('Request failed.  Returned status of ' + xhr.status);
+          }
+      };
+      xhr.send();
+    });
+  });
+
+  //Gives all admins of the event
+  router.get('event/admins/:id', function(req, res){
+    var eventID = req.params.id;
+    var sessionID = "10206500617488421";
+    User.findOne({'fbID': sessionID}, function(err, user){
+      console.log(user.token);
+      var uri = "https://graph.facebook.com/"+eventID+"/admins?access_token="+user.token;
+      console.log(uri);
+
+      // GET Request in native JavaScript
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', encodeURI(uri));
+      xhr.onload = function() {
+          if (xhr.status === 200) {
+              res.json(JSON.parse(xhr.responseText));
+          }
+          else {
+              console.log('Request failed.  Returned status of ' + xhr.status);
+          }
+      };
+      xhr.send();
+    });
+  });
+
 
 // Return router
 module.exports = router;
