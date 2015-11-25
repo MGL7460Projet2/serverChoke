9a3ee8389776221a6d266b5e35134104159a71a4
Victor <victor.dupuy@hei.fr>
2015-11-25 14:11:18 -0500

    little change

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,7 @@ module.exports = {
   'facebookAuth' : {
     'clientID' : '1093913377287097',
     'clientSecret' : 'd27d458ff5f09cad2bcfabd37729ecae',
-    'callbackURL' : 'https://shrouded-harbor-6203.herokuapp.com/api/auth/facebook/callback'
+  //  'callbackURL' : 'https://shrouded-harbor-6203.herokuapp.com/api/auth/facebook/callback'
+    'callbackURL' : 'http://localhost/'
   }
 }
