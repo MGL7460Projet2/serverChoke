db87e441562b17c2548d60192410849462111222
Victor <victor.dupuy@hei.fr>
2015-11-25 14:22:22 -0500

    This should work

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,6 @@ module.exports = {
   'facebookAuth' : {
     'clientID' : '1093913377287097',
     'clientSecret' : 'd27d458ff5f09cad2bcfabd37729ecae',
-  //  'callbackURL' : 'https://shrouded-harbor-6203.herokuapp.com/api/auth/facebook/callback'
-    'callbackURL' : 'http://localhost/'
+    'callbackURL' : 'https://shrouded-harbor-6203.herokuapp.com/api/auth/facebook/callback'
   }
 }
