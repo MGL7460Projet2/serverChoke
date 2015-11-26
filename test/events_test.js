67f0c1369225186e00823422b683281995a690c0
lamrous <lamrous.lyes@gmail.com>
2015-11-26 15:37:49 -0500

    Modification des tests mocha+chai

--- test/events_test.js
+++ test/events_test.js
@@ -1,4 +1,4 @@
-describe('Events', function () {
+describe('Chokes', function () {
 
 var should = require('chai').should(),
 expect = require('chai').expect,
@@ -6,31 +6,21 @@ supertest = require('supertest'),
 api = supertest('http://localhost:3000');
 
 it('should return a 200 response', function(done) {
-   api.get('/api/events')
+   api.get('/api/chokes')
    .set('Accept', 'application/json')
    .expect(200,done)
 });
 
-
-it('should be an object with keys and values', function(done) {
-   api.get('/api/events/5646a8ebfa17227c161273d5')
-   .set('Accept', 'application/json')
-   .expect(200)
-   .end(function(err, res) {
-      expect(res.body).to.have.property("name");
-	  expect(res.body.name).to.not.equal(null);
-	  expect(res.body).to.have.property("face");
-	  expect(res.body.face).to.not.equal(null);
-	  expect(res.body).to.have.property("lastText");
-	  expect(res.body.lastText).to.not.equal(null);
-	  done();
-	});
-});
-
-it('should create a new event', function(done) {
-   api.post('/api/events')
+it('should create a new choke', function(done) {
+   api.post('/api/chokes')
    .set('Accept', 'application/x-www-from-urlencoded')
-   .send({name: "bbba", face: "face", lastText: "aaa bbb ccc ddd eee"})
+   .send({
+     fbSender: '22',
+     fbReceiver: '11',
+     event: [],
+     answered : false,
+     response : false
+   })
    .expect('Content-Type', /json/)
    .expect(200)
    .end(function(err, res){
@@ -40,22 +30,44 @@ it('should create a new event', function(done) {
 });
 
 
-
-it('should be updated with a new name', function(done) {
-   api.put('/api/events/5646a8ebfa17227c161273d5')
-   .set('Accept', 'application/x-www-from-urlencoded')
-   .send({name: "Kevin a", face: "face", lastText: "aaa bbb ccc"})
+it('should be an object with keys and values', function(done) {
+   api.get('/api/chokes/5646a8ebfa17227c161273d5')
+   .set('Accept', 'application/json')
    .expect(200)
    .end(function(err, res) {
-      expect(res.body.name).to.equal("Kevin a");
-	  expect(res.body.face).to.equal("face");
-	  expect(res.body.lastText).to.equal("aaa bbb ccc");
+    expect(res.body).to.have.property("fbSender");
+	  expect(res.body.fbSender).to.not.equal(null);
+	  expect(res.body).to.have.property("fbReceiver");
+	  expect(res.body.fbReceiver).to.not.equal(null);
+	  expect(res.body).to.have.property("answered");
+	  expect(res.body.answered).to.not.equal(null);
+	  done();
+	});
+});
+
+
+it('should be updated with a new answred value', function(done) {
+   api.put('/api/chokes/5646a8ebfa17227c161273d5')
+   .set('Accept', 'application/x-www-from-urlencoded')
+   .send({
+     fbSender: '22',
+     fbReceiver: '11',
+     event: [],
+     answered : true,
+     response : false
+   })
+   .expect(200)
+   .end(function(err, res) {
+    expect(res.body.answered).to.equal(true);
+	  expect(res.body.response).to.equal(false);
+    expect(res.body.fbSender).to.equal("22");
+	  expect(res.body.fbReceiver).to.equal("11");
 	  done();
 	});
   });
 
 it('should respond 200',function(done){
-	api.del('/api/events/564695e7910aa2041772649e')
+	api.del('/api/chokes/5646a8ebfa17227c161273d5')
 	.set('Accept', 'application/json')
 	.expect(200)
 	.end(function(err, res) {
